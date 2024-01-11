f020e5c53424afb5cf18ca00b9c1963592dcfe94
eonepp <vaniapopov2004@mail.ru>
2024-01-11 21:17:10 +0300

    добавление иконки сердца

--- DatingProgram/Forms/NewUserWindow.Designer.cs
+++ DatingProgram/Forms/NewUserWindow.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewUserWindow));
             this.mainTitleLabel = new System.Windows.Forms.Label();
             this.genderLabel = new System.Windows.Forms.Label();
             this.checkBoxMale = new System.Windows.Forms.CheckBox();
@@ -50,9 +51,10 @@
             this.mainTitleLabel.AutoSize = true;
             this.mainTitleLabel.Font = new System.Drawing.Font("Arial Narrow", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.mainTitleLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.mainTitleLabel.Location = new System.Drawing.Point(12, 9);
+            this.mainTitleLabel.Location = new System.Drawing.Point(9, 7);
+            this.mainTitleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.mainTitleLabel.Name = "mainTitleLabel";
-            this.mainTitleLabel.Size = new System.Drawing.Size(519, 69);
+            this.mainTitleLabel.Size = new System.Drawing.Size(414, 57);
             this.mainTitleLabel.TabIndex = 0;
             this.mainTitleLabel.Text = "Заполните пофиль";
             // 
@@ -60,9 +62,10 @@
             // 
             this.genderLabel.AutoSize = true;
             this.genderLabel.Font = new System.Drawing.Font("Arial", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.genderLabel.Location = new System.Drawing.Point(34, 91);
+            this.genderLabel.Location = new System.Drawing.Point(26, 74);
+            this.genderLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.genderLabel.Name = "genderLabel";
-            this.genderLabel.Size = new System.Drawing.Size(74, 34);
+            this.genderLabel.Size = new System.Drawing.Size(58, 28);
             this.genderLabel.TabIndex = 1;
             this.genderLabel.Text = "Пол";
             // 
@@ -70,9 +73,10 @@
             // 
             this.checkBoxMale.AutoSize = true;
             this.checkBoxMale.Font = new System.Drawing.Font("Arial Narrow", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.checkBoxMale.Location = new System.Drawing.Point(122, 91);
+            this.checkBoxMale.Location = new System.Drawing.Point(92, 74);
+            this.checkBoxMale.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBoxMale.Name = "checkBoxMale";
-            this.checkBoxMale.Size = new System.Drawing.Size(145, 39);
+            this.checkBoxMale.Size = new System.Drawing.Size(119, 33);
             this.checkBoxMale.TabIndex = 4;
             this.checkBoxMale.Text = "Мужской";
             this.checkBoxMale.UseVisualStyleBackColor = true;
@@ -82,9 +86,10 @@
             // 
             this.checkBoxFemale.AutoSize = true;
             this.checkBoxFemale.Font = new System.Drawing.Font("Arial Narrow", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.checkBoxFemale.Location = new System.Drawing.Point(122, 136);
+            this.checkBoxFemale.Location = new System.Drawing.Point(92, 110);
+            this.checkBoxFemale.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBoxFemale.Name = "checkBoxFemale";
-            this.checkBoxFemale.Size = new System.Drawing.Size(145, 39);
+            this.checkBoxFemale.Size = new System.Drawing.Size(118, 33);
             this.checkBoxFemale.TabIndex = 5;
             this.checkBoxFemale.Text = "Женский";
             this.checkBoxFemale.UseVisualStyleBackColor = true;
@@ -94,9 +99,10 @@
             // 
             this.ageLabel.AutoSize = true;
             this.ageLabel.Font = new System.Drawing.Font("Arial Narrow", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ageLabel.Location = new System.Drawing.Point(18, 589);
+            this.ageLabel.Location = new System.Drawing.Point(14, 479);
+            this.ageLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ageLabel.Name = "ageLabel";
-            this.ageLabel.Size = new System.Drawing.Size(211, 35);
+            this.ageLabel.Size = new System.Drawing.Size(170, 29);
             this.ageLabel.TabIndex = 6;
             this.ageLabel.Text = "Дата рождения";
             // 
@@ -104,9 +110,10 @@
             // 
             this.nameLabel.AutoSize = true;
             this.nameLabel.Font = new System.Drawing.Font("Arial Narrow", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.nameLabel.Location = new System.Drawing.Point(43, 190);
+            this.nameLabel.Location = new System.Drawing.Point(32, 154);
+            this.nameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(65, 35);
+            this.nameLabel.Size = new System.Drawing.Size(54, 29);
             this.nameLabel.TabIndex = 8;
             this.nameLabel.Text = "Имя";
             // 
@@ -114,9 +121,10 @@
             // 
             this.aboutPersonLabel.AutoSize = true;
             this.aboutPersonLabel.Font = new System.Drawing.Font("Arial Narrow", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.aboutPersonLabel.Location = new System.Drawing.Point(18, 286);
+            this.aboutPersonLabel.Location = new System.Drawing.Point(14, 232);
+            this.aboutPersonLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.aboutPersonLabel.Name = "aboutPersonLabel";
-            this.aboutPersonLabel.Size = new System.Drawing.Size(106, 35);
+            this.aboutPersonLabel.Size = new System.Drawing.Size(85, 29);
             this.aboutPersonLabel.TabIndex = 9;
             this.aboutPersonLabel.Text = "О себе:";
             // 
@@ -124,9 +132,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Narrow", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(18, 228);
+            this.label1.Location = new System.Drawing.Point(14, 185);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 35);
+            this.label1.Size = new System.Drawing.Size(73, 29);
             this.label1.TabIndex = 10;
             this.label1.Text = "Город";
             // 
@@ -134,19 +143,21 @@
             // 
             this.contractLabel.AutoSize = true;
             this.contractLabel.Font = new System.Drawing.Font("Arial Narrow", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.contractLabel.Location = new System.Drawing.Point(18, 434);
+            this.contractLabel.Location = new System.Drawing.Point(14, 353);
+            this.contractLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.contractLabel.Name = "contractLabel";
-            this.contractLabel.Size = new System.Drawing.Size(320, 35);
+            this.contractLabel.Size = new System.Drawing.Size(257, 29);
             this.contractLabel.TabIndex = 11;
             this.contractLabel.Text = "Требования к партнёру:";
             // 
             // textBoxAboutMe
             // 
             this.textBoxAboutMe.Font = new System.Drawing.Font("Arial Narrow", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxAboutMe.Location = new System.Drawing.Point(24, 324);
+            this.textBoxAboutMe.Location = new System.Drawing.Point(18, 263);
+            this.textBoxAboutMe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxAboutMe.MaxLength = 150;
             this.textBoxAboutMe.Name = "textBoxAboutMe";
-            this.textBoxAboutMe.Size = new System.Drawing.Size(507, 96);
+            this.textBoxAboutMe.Size = new System.Drawing.Size(381, 79);
             this.textBoxAboutMe.TabIndex = 13;
             this.textBoxAboutMe.Text = "";
             this.textBoxAboutMe.TextChanged += new System.EventHandler(this.textBoxAboutMe_TextChanged);
@@ -154,10 +165,11 @@
             // textBoxContract
             // 
             this.textBoxContract.Font = new System.Drawing.Font("Arial Narrow", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxContract.Location = new System.Drawing.Point(24, 473);
+            this.textBoxContract.Location = new System.Drawing.Point(18, 384);
+            this.textBoxContract.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxContract.MaxLength = 150;
             this.textBoxContract.Name = "textBoxContract";
-            this.textBoxContract.Size = new System.Drawing.Size(507, 96);
+            this.textBoxContract.Size = new System.Drawing.Size(381, 79);
             this.textBoxContract.TabIndex = 14;
             this.textBoxContract.Text = "";
             this.textBoxContract.TextChanged += new System.EventHandler(this.textBoxContract_TextChanged);
@@ -165,45 +177,51 @@
             // textBoxName
             // 
             this.textBoxName.Font = new System.Drawing.Font("Arial Narrow", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxName.Location = new System.Drawing.Point(122, 187);
+            this.textBoxName.Location = new System.Drawing.Point(92, 152);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxName.MaxLength = 28;
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(409, 42);
+            this.textBoxName.Size = new System.Drawing.Size(308, 35);
             this.textBoxName.TabIndex = 15;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
             // 
             // textBoxCity
             // 
             this.textBoxCity.Font = new System.Drawing.Font("Arial Narrow", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxCity.Location = new System.Drawing.Point(122, 228);
+            this.textBoxCity.Location = new System.Drawing.Point(92, 185);
+            this.textBoxCity.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxCity.MaxLength = 28;
             this.textBoxCity.Name = "textBoxCity";
-            this.textBoxCity.Size = new System.Drawing.Size(409, 42);
+            this.textBoxCity.Size = new System.Drawing.Size(308, 35);
             this.textBoxCity.TabIndex = 16;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Arial Narrow", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(85, 655);
+            this.button1.Location = new System.Drawing.Point(64, 532);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(386, 56);
+            this.button1.Size = new System.Drawing.Size(290, 46);
             this.button1.TabIndex = 17;
             this.button1.Text = "Создать профиль";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Font = new System.Drawing.Font("Arial Narrow", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(251, 592);
+            this.dateTimePicker1.Location = new System.Drawing.Point(188, 481);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(280, 30);
+            this.dateTimePicker1.Size = new System.Drawing.Size(211, 26);
             this.dateTimePicker1.TabIndex = 18;
             // 
             // NewUserWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(556, 749);
+            this.ClientSize = new System.Drawing.Size(417, 609);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBoxCity);
@@ -219,8 +237,11 @@
             this.Controls.Add(this.checkBoxMale);
             this.Controls.Add(this.genderLabel);
             this.Controls.Add(this.mainTitleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "NewUserWindow";
             this.Text = "Создание профиля";
+            this.Load += new System.EventHandler(this.NewUserWindow_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- DatingProgram/Forms/NewUserWindow.cs
+++ DatingProgram/Forms/NewUserWindow.cs
@@ -1,13 +1,17 @@
 ﻿using DatingProgram.Data;
 using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics.Contracts;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace DatingProgram.Forms
 {
     public partial class NewUserWindow : Form
     {
         public bool CreatedSuccessfully => createdSuccessfully;
-
+        
         // это надо сделать true, если запись удалось добавить
         private bool createdSuccessfully = false;
 
@@ -70,6 +74,23 @@ namespace DatingProgram.Forms
         }
 
         private void textBoxAboutMe_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewUserWindow_Load(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            actualProfilesBase.OpenConnection();
+
+            SqlCommand Command = new SqlCommand("insert into ActualProfiles(regid, regdate, gender, name, birthDay, city, description, contract) values(2, '2024-01-07 12:30:30:00', N'Ж', N'Натали Портамн', '1981-06-09', N'', N'Монтесито', N'Актриса', N'Ищу сильного мужчину, на которого можно опереться')");
+            Command.Parameters.Add("@regid", Convert.ToString(id));
+            createdSuccessfully = true;
+            Command.ExecuteNonQuery();
+        }
+        private void textBoxName_TextChanged(object sender, EventArgs e)
         {
 
         }
